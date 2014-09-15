5d315cc1991c81bd6867480a4fc79f252079a1a6
Nicole Aitchison <nlaitchison@gmail.com>
2014-09-15 14:13:07 -0500

    added soft scroll

--- basinHoldings/js/main.js
+++ basinHoldings/js/main.js
@@ -1,6 +1,8 @@
 $(function(){
 
-	console.log('meow');
+	console.log('running');
+
+	// Companies Hover Effects
 
 	$(".company.supply").hover(
 		function() {
@@ -41,5 +43,20 @@ $(function(){
 			$(".company.energy .company-text").removeClass( "current" );
 		}
 	);
+
+	// SMOOTH SCROLLING
+
+	$('a[href*=#]:not([href=#])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      if (target.length) {
+	        $('html,body').animate({
+	          scrollTop: target.offset().top
+	        }, 1000);
+	        return false;
+	      }
+	    }
+  	});
 
 });
