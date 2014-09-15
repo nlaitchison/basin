7ac94341855ac91f17b4cd5b041d60a0c6d24984
Nicole Aitchison <nlaitchison@gmail.com>
2014-09-15 13:58:40 -0500

    added css animations with jquery hover

--- basinHoldings/js/main.js
+++ basinHoldings/js/main.js
@@ -1,4 +1,45 @@
 $(function(){
 
+	console.log('meow');
+
+	$(".company.supply").hover(
+		function() {
+			console.log('over');
+			$(".company.supply .company-text").addClass( "current" );
+		}, function() {
+			console.log('off');
+			$(".company.supply .company-text").removeClass( "current" );
+		}
+	);
+
+	$(".company.rentals").hover(
+		function() {
+			console.log('over');
+			$(".company.rentals .company-text").addClass( "current" );
+		}, function() {
+			console.log('off');
+			$(".company.rentals .company-text").removeClass( "current" );
+		}
+	);
+
+	$(".company.tools").hover(
+		function() {
+			console.log('over');
+			$(".company.tools .company-text").addClass( "current" );
+		}, function() {
+			console.log('off');
+			$(".company.tools .company-text").removeClass( "current" );
+		}
+	);
+
+	$(".company.energy").hover(
+		function() {
+			console.log('over');
+			$(".company.energy .company-text").addClass( "current" );
+		}, function() {
+			console.log('off');
+			$(".company.energy .company-text").removeClass( "current" );
+		}
+	);
 
 });
